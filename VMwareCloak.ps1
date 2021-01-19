151fa53194edf2c6cd475693a824510d9dc790fa
Kyle Cucci <kyle.cucci@gmail.com>
2021-01-19 16:28:12 +0100

    Added random string generator!
    Added random string generator, and removed old static strings.

--- VMwareCloak.ps1
+++ VMwareCloak.ps1
@@ -4,7 +4,7 @@
 ## Many thanks to pafish for some of these ideas :) - https://github.com/a0rtega/pafish
 ##################################################
 ## Author: d4rksystem
-## Version: 0.1
+## Version: 0.2
 ##################################################
 
 # Define command line parameters
@@ -32,6 +32,18 @@ Write-Output 'procs: Kill processes.'
 Write-Output 'files: Make file system changes.'
 Write-Output 'Make sure to run as Admin!'
 Write-Output '*****************************************'
+
+# Define random string generator function
+function Get-RandomString {
+
+    $charSet = "abcdefghijklmnopqrstuvwxyz0123456789".ToCharArray()
+    
+    for ($i = 0; $i -lt 10; $i++ ) {
+        $randomString += $charSet | Get-Random
+    }
+
+    return $randomString
+}
 
 # Stop VMware Processes
 if ($procs) {
@@ -80,19 +92,19 @@ if ($reg) {
     if (Get-ItemProperty -Path "HKLM:\HARDWARE\Description\System" -Name "SystemBiosVersion" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Modifying Reg Key HKLM:\HARDWARE\Description\System\SystemBiosVersion..."
-        Set-ItemProperty -Path "HKLM:\HARDWARE\Description\System" -Name "SystemBiosVersion" -Value "LOLBIOSv2"
+        Set-ItemProperty -Path "HKLM:\HARDWARE\Description\System" -Name "SystemBiosVersion" -Value $(Get-RandomString)
 
         Write-Output "[*] Modifying Reg Key HKLM:\HARDWARE\Description\System\BIOS\BIOSVendor..."
-        Set-ItemProperty -Path "HKLM:\HARDWARE\Description\System\BIOS" -Name "BIOSVendor" -Value "Nope, Inc."
+        Set-ItemProperty -Path "HKLM:\HARDWARE\Description\System\BIOS" -Name "BIOSVendor" -Value $(Get-RandomString)
 
         Write-Output "[*] Modifying Reg Key HKLM:\HARDWARE\Description\System\BIOS\BIOSVersion..."
-        Set-ItemProperty -Path "HKLM:\HARDWARE\Description\System\BIOS" -Name "BIOSVersion" -Value "LOLBIOSv2"
+        Set-ItemProperty -Path "HKLM:\HARDWARE\Description\System\BIOS" -Name "BIOSVersion" -Value $(Get-RandomString)
 
         Write-Output "[*] Modifying Reg Key HKLM:\HARDWARE\Description\System\BIOS\SystemManufacturer..."
-        Set-ItemProperty -Path "HKLM:\HARDWARE\Description\System\BIOS" -Name "SystemManufacturer" -Value "Nope, Inc."
+        Set-ItemProperty -Path "HKLM:\HARDWARE\Description\System\BIOS" -Name "SystemManufacturer" -Value $(Get-RandomString)
 
         Write-Output "[*] Modifying Reg Key HKLM:\HARDWARE\Description\System\BIOS\SystemProductName..."
-        Set-ItemProperty -Path "HKLM:\HARDWARE\Description\System\BIOS" -Name "SystemProductName" -Value "LOLBIOSv2"
+        Set-ItemProperty -Path "HKLM:\HARDWARE\Description\System\BIOS" -Name "SystemProductName" -Value $(Get-RandomString)
 
     } Else {
 
@@ -126,7 +138,7 @@ if ($reg) {
     if (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Store\Configuration" -Name "OEMID" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Modifying Reg Key HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Store\Configuration\OEMID"
-        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Store\Configuration" -Name "OEMID" -Value "NOPE"
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Store\Configuration" -Name "OEMID" -Value $(Get-RandomString)
 
     } Else {
 
@@ -138,7 +150,7 @@ if ($reg) {
     if (Get-Item -Path "HKLM:\SOFTWARE\Classes\Applications\VMwareHostOpen.exe" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Modifying Reg Key HKLM:\SOFTWARE\Classes\Applications\VMwareHostOpen.exe"
-        Rename-Item -Path "HKLM:\SOFTWARE\Classes\Applications\VMwareHostOpen.exe" -NewName "Nope.exe"
+        Rename-Item -Path "HKLM:\SOFTWARE\Classes\Applications\VMwareHostOpen.exe" -NewName $(Get-RandomString)
 
     } Else {
 
@@ -150,7 +162,7 @@ if ($reg) {
     if (Get-Item -Path "HKLM:\SOFTWARE\Classes\VMwareHostOpen.AssocURL" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Modifying Reg Key HKLM:\SOFTWARE\Classes\VMwareHostOpen.AssocURL"
-        Rename-Item -Path "HKLM:\SOFTWARE\Classes\VMwareHostOpen.AssocURL" -NewName "Nope.AssocURL"
+        Rename-Item -Path "HKLM:\SOFTWARE\Classes\VMwareHostOpen.AssocURL" -NewName $(Get-RandomString)
 
     } Else {
 
@@ -162,7 +174,7 @@ if ($reg) {
     if (Get-Item -Path "HKLM:\SOFTWARE\Classes\VMwareHostOpen.AssocFile" -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Modifying Reg Key HKLM:\SOFTWARE\Classes\VMwareHostOpen.AssocFile"
-        Rename-Item -Path "HKLM:\SOFTWARE\Classes\VMwareHostOpen.AssocFile" -NewName "Nope.AssocFile"
+        Rename-Item -Path "HKLM:\SOFTWARE\Classes\VMwareHostOpen.AssocFile" -NewName $(Get-RandomString)
 
     } Else {
 
@@ -174,7 +186,7 @@ if ($reg) {
     if (Get-Item -Path "HKCU:\SOFTWARE\VMware, Inc." -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Modifying Reg Key HKCU:\SOFTWARE\VMware, Inc."
-        Rename-Item -Path "HKCU:\SOFTWARE\VMware, Inc." -NewName "Nope, Inc."
+        Rename-Item -Path "HKCU:\SOFTWARE\VMware, Inc." -NewName $(Get-RandomString)
 
     } Else {
 
@@ -184,7 +196,7 @@ if ($reg) {
     if (Get-Item -Path "HKLM:\SOFTWARE\VMware, Inc." -ErrorAction SilentlyContinue) {
 
         Write-Output "[*] Modifying Reg Key HKLM:\SOFTWARE\VMware, Inc."
-        Rename-Item -Path "HKLM:\SOFTWARE\VMware, Inc." -NewName "Nope, Inc."
+        Rename-Item -Path "HKLM:\SOFTWARE\VMware, Inc." -NewName $(Get-RandomString)
 
     } Else {
 
@@ -223,7 +235,7 @@ if ($files) {
     $VMwareCommonFiles = "C:\Program Files\Common Files\VMware"
 
     if (Test-Path -Path $VMwareCommonFiles) {
-        Rename-Item $VMwareCommonFiles "C:\Program Files\Common Files\Nope"
+        Rename-Item $VMwareCommonFiles "C:\Program Files\Common Files\$(Get-RandomString)"
     }
 
     else {
@@ -235,7 +247,7 @@ if ($files) {
     $VMwareProgramDir = "C:\Program Files\VMware"
 
     if (Test-Path -Path $VMwareProgramDir) {
-        Rename-Item $VMwareProgramDir "C:\Program Files\Nope"
+        Rename-Item $VMwareProgramDir "C:\Program Files\$(Get-RandomString)"
     }
 
      else {
@@ -243,58 +255,58 @@ if ($files) {
     }
 
     Write-Output '[*] Attempting to rename VMware driver files in C:\Windows\System32\drivers\...'
+	
     # We are renaming these files, as opposed to removing them, because Windows doesn't care if we just rename them :)
-
-    Rename-Item "C:\Windows\System32\drivers\vm3dmp-debug.sys" "C:\Windows\System32\drivers\nope1.sys"
-    Rename-Item "C:\Windows\System32\drivers\vm3dmp-stats.sys" "C:\Windows\System32\drivers\nope2.sys"
-    Rename-Item "C:\Windows\System32\drivers\vm3dmp.sys" "C:\Windows\System32\drivers\nope3.sys"
-    Rename-Item "C:\Windows\System32\drivers\vm3dmp_loader.sys" "C:\Windows\System32\drivers\nope4.sys"
-    Rename-Item "C:\Windows\System32\drivers\vmhgfs.sys" "C:\Windows\System32\drivers\nope5.sys"
-    Rename-Item "C:\Windows\System32\drivers\vmmemctl.sys" "C:\Windows\System32\drivers\nope6.sys"
-    Rename-Item "C:\Windows\System32\drivers\vmmouse.sys" "C:\Windows\System32\drivers\nope7.sys"
-    Rename-Item "C:\Windows\System32\drivers\vmrawdsk.sys" "C:\Windows\System32\drivers\nope8.sys"
-    Rename-Item "C:\Windows\System32\drivers\vmusbmouse.sys" "C:\Windows\System32\drivers\nope9.sys"
+    Rename-Item "C:\Windows\System32\drivers\vm3dmp-debug.sys" "C:\Windows\System32\drivers\$(Get-RandomString).sys"
+    Rename-Item "C:\Windows\System32\drivers\vm3dmp-stats.sys" "C:\Windows\System32\drivers\$(Get-RandomString).sys"
+    Rename-Item "C:\Windows\System32\drivers\vm3dmp.sys" "C:\Windows\System32\drivers\$(Get-RandomString).sys"
+    Rename-Item "C:\Windows\System32\drivers\vm3dmp_loader.sys" "C:\Windows\System32\drivers\$(Get-RandomString).sys"
+    Rename-Item "C:\Windows\System32\drivers\vmhgfs.sys" "C:\Windows\System32\drivers\$(Get-RandomString).sys"
+    Rename-Item "C:\Windows\System32\drivers\vmmemctl.sys" "C:\Windows\System32\drivers\$(Get-RandomString).sys"
+    Rename-Item "C:\Windows\System32\drivers\vmmouse.sys" "C:\Windows\System32\drivers\$(Get-RandomString).sys"
+    Rename-Item "C:\Windows\System32\drivers\vmrawdsk.sys" "C:\Windows\System32\drivers\$(Get-RandomString).sys"
+    Rename-Item "C:\Windows\System32\drivers\vmusbmouse.sys" "C:\Windows\System32\drivers\$(Get-RandomString).sys"
 
     Write-Output '[*] Attempting to rename system files in C:\Windows\System32\...'
 
-    Rename-Item "C:\Windows\System32\vm3dco.dll" "C:\Windows\System32\nope1.dll"
-    Rename-Item "C:\Windows\System32\vm3ddevapi64-debug.dll" "C:\Windows\System32\nope2.dll"
-    Rename-Item "C:\Windows\System32\vm3ddevapi64-release.dll" "C:\Windows\System32\nope3.dll"
-    Rename-Item "C:\Windows\System32\vm3ddevapi64-stats.dll" "C:\Windows\System32\nope4.dll"
-    Rename-Item "C:\Windows\System32\vm3ddevapi64.dll" "C:\Windows\System32\nope5.dll"
-    Rename-Item "C:\Windows\System32\vm3dgl64.dll" "C:\Windows\System32\nope6.dll"
-    Rename-Item "C:\Windows\System32\vm3dglhelper64.dll" "C:\Windows\System32\nope7.dll"
-    Rename-Item "C:\Windows\System32\vm3dservice.exe" "C:\Windows\System32\nope.exe"
-    Rename-Item "C:\Windows\System32\vm3dum64-debug.dll" "C:\Windows\System32\nope8.dll"
-    Rename-Item "C:\Windows\System32\vm3dum64-stats.dll" "C:\Windows\System32\nope9.dll"
-    Rename-Item "C:\Windows\System32\vm3dum64.dll" "C:\Windows\System32\nope10.dll"
-    Rename-Item "C:\Windows\System32\vm3dum64_10-debug.dll" "C:\Windows\System32\nope11.dll"
-    Rename-Item "C:\Windows\System32\vm3dum64_10-stats.dll" "C:\Windows\System32\nope12.dll"
-    Rename-Item "C:\Windows\System32\vm3dum64_10.dll" "C:\Windows\System32\nope13.dll"
-    Rename-Item "C:\Windows\System32\vm3dum64_loader.dll" "C:\Windows\System32\nope14.dll"
-    Rename-Item "C:\Windows\System32\vmGuestLib.dll" "C:\Windows\System32\nope15.dll"
-    Rename-Item "C:\Windows\System32\vmGuestLibJava.dll" "C:\Windows\System32\nope16.dll"
-    Rename-Item "C:\Windows\System32\vmhgfs.dll" "C:\Windows\System32\nope17.dll"
-    Rename-Item "C:\Windows\System32\VMWSU.DLL" "C:\Windows\System32\nope18.dll"
+    Rename-Item "C:\Windows\System32\vm3dco.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\vm3ddevapi64-debug.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\vm3ddevapi64-release.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\vm3ddevapi64-stats.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\vm3ddevapi64.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\vm3dgl64.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\vm3dglhelper64.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\vm3dservice.exe" "C:\Windows\System32\$(Get-RandomString).exe"
+    Rename-Item "C:\Windows\System32\vm3dum64-debug.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\vm3dum64-stats.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\vm3dum64.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\vm3dum64_10-debug.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\vm3dum64_10-stats.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\vm3dum64_10.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\vm3dum64_loader.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\vmGuestLib.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\vmGuestLibJava.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\vmhgfs.dll" "C:\Windows\System32\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\System32\VMWSU.DLL" "C:\Windows\System32\$(Get-RandomString).dll"
 
     Write-Output '[*] Attempting to rename system files in C:\Windows\SysWOW64\...'
 
-    Rename-Item "C:\Windows\SysWOW64\vm3ddevapi-debug.dll" "C:\Windows\SysWOW64\nope1.dll"
-    Rename-Item "C:\Windows\SysWOW64\vm3ddevapi-release.dll" "C:\Windows\SysWOW64\nope2.dll"
-    Rename-Item "C:\Windows\SysWOW64\vm3ddevapi-stats.dll" "C:\Windows\SysWOW64\nope3.dll"
-    Rename-Item "C:\Windows\SysWOW64\vm3ddevapi.dll" "C:\Windows\SysWOW64\nope4.dll"
-    Rename-Item "C:\Windows\SysWOW64\vm3dgl.dll" "C:\Windows\SysWOW64\nope5.dll"
-    Rename-Item "C:\Windows\SysWOW64\vm3dglhelper.dll" "C:\Windows\SysWOW64\nope6.dll"
-    Rename-Item "C:\Windows\SysWOW64\vm3dum-debug.dll" "C:\Windows\SysWOW64\nope7.dll"
-    Rename-Item "C:\Windows\SysWOW64\vm3dum-stats.dll" "C:\Windows\SysWOW64\nope8.dll"
-    Rename-Item "C:\Windows\SysWOW64\vm3dum.dll" "C:\Windows\SysWOW64\nope9.dll"
-    Rename-Item "C:\Windows\SysWOW64\vm3dum_10-debug.dll" "C:\Windows\SysWOW64\nope10.dll"
-    Rename-Item "C:\Windows\SysWOW64\vm3dum_10-stats.dll" "C:\Windows\SysWOW64\nope11.dll"
-    Rename-Item "C:\Windows\SysWOW64\vm3dum_10.dll" "C:\Windows\SysWOW64\nope12.dll"
-    Rename-Item "C:\Windows\SysWOW64\vm3dum_loader.dll" "C:\Windows\SysWOW64\nope13.dll"
-    Rename-Item "C:\Windows\SysWOW64\vmGuestLib.dll" "C:\Windows\SysWOW64\nope14.dll"
-    Rename-Item "C:\Windows\SysWOW64\vmGuestLibJava.dll" "C:\Windows\SysWOW64\nope15.dll"
-    Rename-Item "C:\Windows\SysWOW64\vmhgfs.dll" "C:\Windows\SysWOW64\nope16.dll" 
+    Rename-Item "C:\Windows\SysWOW64\vm3ddevapi-debug.dll" "C:\Windows\SysWOW64\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\SysWOW64\vm3ddevapi-release.dll" "C:\Windows\SysWOW64\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\SysWOW64\vm3ddevapi-stats.dll" "C:\Windows\SysWOW64\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\SysWOW64\vm3ddevapi.dll" "C:\Windows\SysWOW64\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\SysWOW64\vm3dgl.dll" "C:\Windows\SysWOW64\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\SysWOW64\vm3dglhelper.dll" "C:\Windows\SysWOW64\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\SysWOW64\vm3dum-debug.dll" "C:\Windows\SysWOW64\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\SysWOW64\vm3dum-stats.dll" "C:\Windows\SysWOW64\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\SysWOW64\vm3dum.dll" "C:\Windows\SysWOW64\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\SysWOW64\vm3dum_10-debug.dll" "C:\Windows\SysWOW64\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\SysWOW64\vm3dum_10-stats.dll" "C:\Windows\SysWOW64\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\SysWOW64\vm3dum_10.dll" "C:\Windows\SysWOW64\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\SysWOW64\vm3dum_loader.dll" "C:\Windows\SysWOW64\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\SysWOW64\vmGuestLib.dll" "C:\Windows\SysWOW64\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\SysWOW64\vmGuestLibJava.dll" "C:\Windows\SysWOW64\$(Get-RandomString).dll"
+    Rename-Item "C:\Windows\SysWOW64\vmhgfs.dll" "C:\Windows\SysWOW64\$(Get-RandomString).dll" 
 }
 
 Write-Output '** Done! Did you recieve a lot of errors? Try running as Admin!'
